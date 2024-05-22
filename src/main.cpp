2fc35ee07f346f3e01c77ae046df898f41ef9cd2
Paul <paul.noiroux@gmail.com>
2024-05-22 13:52:04 +0200

    +Not finished fade.
    +Not finished fade.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@ int main()
 {
     gl::init("TPs de Rendering");
     gl::maximize_window();
-
+    glEnable(GL_BLEND);
+    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_ONE);
     auto const shader = gl::Shader{{
     .vertex   = gl::ShaderSource::File{"res/vertex.glsl"},
     .fragment = gl::ShaderSource::File{"res/fragment.glsl"},
@@ -34,6 +35,9 @@ int main()
         shader.set_uniform("aspect_ratio", gl::framebuffer_aspect_ratio());
         shader.set_uniform("uniformValue", glm::vec2{1.f,3.f});
         shader.set_uniform("movement", gl::time_in_seconds());
+
+        //TODO set fade
+        //shader.set_uniform("colorFade", glm::vec4{1.0f,1.0f,1.0f, ALPHAVALUE});
         rectangle_mesh.draw();
     }
 }
