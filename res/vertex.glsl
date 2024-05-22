c846cb055a1e50fa46a0b004229ee3de0835aef1
Paul <paul.noiroux@gmail.com>
2024-05-22 12:20:39 +0200

    +Modified position (+0.4f, +0.4f)
    +Modified position (+0.4f, +0.4f)

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -4,5 +4,9 @@ layout(location = 0) in vec2 in_position;
 
 void main()
 {
-    gl_Position = vec4(in_position , 0.f ,1.);
+    vec2 position = in_position;
+    
+    position.x = in_position.x + 0.4f;
+    position.y = in_position.y + 0.4f;
+    gl_Position = vec4(position , 0.0f ,1.0f);
 }
