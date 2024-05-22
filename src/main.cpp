5639e90c1750e443531961fa2b9bdf0baed12040
Paul <paul.noiroux@gmail.com>
2024-05-22 12:00:50 +0200

    +Added rectangle +Index buffer
    +Added rectangle +Index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
-#include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
+#include "opengl-framework/opengl-framework.hpp"
 
 int main()
 {
-    // Initialisation
-    gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
-    gl::maximize_window(); // On peut la maximiser si on veut
+    gl::init("TPs de Rendering");
+    gl::maximize_window();
 
-    auto const triangle_mesh = gl::Mesh{{
+    auto const rectangle_mesh = gl::Mesh{{
     .vertex_buffers = {{
-        .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+        .layout = {gl::VertexAttribute::Position2D{0}},
         .data   = {
-            -1.f, -1.f, // Position2D du 1er sommet
-            +1.f, -1.f, // Position2D du 2ème sommet
-             0.f, +1.f  // Position2D du 3ème sommet
+            -0.5f, -0.5f,
+            0.5f, -0.5f,
+            0.5f, 0.5f,
+            -0.5f, 0.5f
         },
     }},
+    .index_buffer = {
+        0,1,2,
+        0,2,3
+    }
 }};
 
     while (gl::window_is_open())
@@ -22,6 +26,6 @@ int main()
         glClearColor(0.f,0.f,1.f,1.f);
         glClear(GL_COLOR_BUFFER_BIT);
         gl::bind_default_shader();
-        triangle_mesh.draw();
+        rectangle_mesh.draw();
     }
 }
