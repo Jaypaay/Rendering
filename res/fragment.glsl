51f831e3102e83b08802d489b722896d67b6a0af
Paul <paul.noiroux@gmail.com>
2024-07-13 22:52:49 +0200

    +Trying to fix opacity
    +Trying to fix opacity

--- res/fragment.glsl
+++ res/fragment.glsl
@@ -11,8 +11,14 @@ void main()
 {
     //Light intensity
     vec4 texture_color = texture(sampler, uv);
-    float intensity = dot(normalize(normals), normalize(light_direction)) + 0.3;
-    out_color = texture_color * -intensity;
+    float r = texture_color.rgba.r;
+    float g = texture_color.rgba.g;
+    float b = texture_color.rgba.b;
+    float a = texture_color.rgba.a;
+
+    vec3 textureColorWithoutAlpha = vec3(texture_color.rgba.r,texture_color.rgba.g,texture_color.rgba.b);
+    float intensity = dot(normalize(textureColorWithoutAlpha), normalize(light_direction)) + 0.3;
+    out_color = vec4(textureColorWithoutAlpha * -intensity, texture_color.rgba.a) ;
     //Utilisé avant pour afficher lesnormals du bateau
     //out_color = vec4(normals, 1);
     //out_color = vec4(texture_color.x, texture_color.y, 0., 1.);
